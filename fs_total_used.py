b3407c90c2f88dfe784ebb76f0ad2844d968e67f
ray <2403404953@qq.com>
2017-12-13 11:22:40 +0800

    new file

--- fs_total_used.py
+++ fs_total_used.py
@@ -1,11 +1,22 @@
-#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+import subprocess
 
-def disk_stat():
-    import os
-    hd = {}
-    disk = os.statvfs('/')
-    hd['available'] = disk.f_bsize * disk.f_bavail
-    hd['total'] = disk.f_bsize * disk.f_blocks
-    hd['used'] = disk.f_bsize * disk.f_bfree
-    return hd
-print(disk_stat())
+#Command 1
+def uname_func():
+    uname="uname"
+    uname_arg="-a"
+    print ("Gathering system information with %s command:\n" % uname)
+    subprocess.call([uname,uname_arg])
+
+#command 2
+def disk_func():
+    diskspace = "df"
+    diskspace_arg="-h"
+    print ("Gathering diskspace information %s command:\n" % diskspace)
+    subprocess.call([diskspace,diskspace_arg])
+
+def main():
+    uname_func()
+    disk_func()
+
+main()
